b58a4c3e8bbc46d73a92c196d973e78f1aa8e1bb
Aditi Ghate <107868821+Adiana3308@users.noreply.github.com>
2022-10-15 18:50:40 +0530

    Add files via upload

---
+++ Sorting Algorithms/Selection Sort/selection_sorting.cpp
@@ -0,0 +1,30 @@
+//Repeatedly swap two elements if they are in wrong order.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>array[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(array[i]>array[j])
+            {
+                int temp=array[j];
+                array[j]=array[i];
+                array[i]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    return 0;
+}
